c8be1bc0d4f67fa8ad957a8b81d57c65423df20a
YutoKinoshita <y-kinoshita@grow-up1.co.jp>
2016-07-11 07:05:13 +0000

    done

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ActiveRecord::Base
   has_many :ownerships  , foreign_key: "item_id" , dependent: :destroy
   has_many :users , through: :ownerships
+  
+  has_many :wants,　foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants
+  
+  has_many :haves, foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, 
 end
